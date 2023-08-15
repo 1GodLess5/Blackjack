3c700bfa1bc35d3a63e9a117ac3436bfcb48fa97
1GodLess5 <gacekalbert@seznam.cz>
2023-08-15 21:02:05 +0200

    added the function usersBet() and some TODOs

--- main.py
+++ main.py
@@ -2,17 +2,23 @@ import functions
 import components
 import ageChecker
 
+
+# TODO 1) cardsRender(cardToDraw: str, numberOfcards: int) -> FINISH THIS FUNCTION
+# TODO 2) dealingCards()
+
+
 # welcoming screen
 functions.firstScreen()
-
 # verifying user's age
 ageChecker.main()
-
 # user's agreement to the rules
 functions.rulesConfirmation()
 
 # getting user's balance to play with
-functions.enterBalance()
+usersBalance = functions.enterBalance()
+
+# getting user's bet for this round
+usersBet = functions.usersBet(usersBalance)
 
 
 
