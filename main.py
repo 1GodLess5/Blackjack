13fcec223c262673ab4c2637e28a976f246a184d
1GodLess5 <gacekalbert@seznam.cz>
2023-09-21 23:24:50 +0200

    added new TODO's

--- main.py
+++ main.py
@@ -3,9 +3,9 @@ import components
 import ageChecker
 import dealingCards
 
-# TODO 1) finish usersFunctionality.py, then finish dealingCards.py
-# TODO 2) dont forget to look at the bottom of dealingCards.py to see the rest of TODO's
-
+# TODO 1) CHECK FOR BUGS IN dealingCards.py, DIDN'T TEST IT THROUGH PROCCESS, BECAUSE IT WASN'T POSSIBLE
+# TODO 2) dont forget to merge after finishing, because you are working in dealingCardsRework
+# TODO 3) make nice users decision map (play again, exit, ? maybe login ?)
 # welcoming screen
 # functions.firstScreen()
 # verifying user's age
@@ -16,11 +16,11 @@ import dealingCards
 # getting user's balance to play with
 # usersBalance = functions.enterBalance()
 usersBalance = 500
+usersBet = 25
 # getting user's bet for this round
 # usersBet = functions.usersBet(usersBalance)
-usersBet = 25
-dealingCards.dealingCards(usersBet)
-
+usersBalance = dealingCards.dealingCards(usersBet, usersBalance)
+print(f"Your new balance: {usersBalance}")
 
 
 
