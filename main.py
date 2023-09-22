ecf9c9074d13b326a160b86a7cd967bf8cc131f6
1GodLess5 <gacekalbert@seznam.cz>
2023-09-22 17:45:32 +0200

    deleted testing variables

--- main.py
+++ main.py
@@ -3,22 +3,21 @@ import components
 import ageChecker
 import dealingCards
 
-# TODO 1) CHECK FOR BUGS IN dealingCards.py, DIDN'T TEST IT THROUGH PROCCESS, BECAUSE IT WASN'T POSSIBLE
-# TODO 2) dont forget to merge after finishing, because you are working in dealingCardsRework
-# TODO 3) make nice users decision map (play again, exit, ? maybe login ?)
+
+# TODO 1) make nice users decision map (play again, exit, ? maybe login ?)
+
 # welcoming screen
-# functions.firstScreen()
+functions.firstScreen()
 # verifying user's age
-# ageChecker.main()
+ageChecker.main()
 # user's agreement to the rules
-# functions.rulesConfirmation()
+functions.rulesConfirmation()
 
 # getting user's balance to play with
-# usersBalance = functions.enterBalance()
-usersBalance = 500
-usersBet = 25
+usersBalance = functions.enterBalance()  # testing variables: usersBalance = 500, usersBet = 25
 # getting user's bet for this round
-# usersBet = functions.usersBet(usersBalance)
+usersBet = functions.usersBet(usersBalance)
+# main function of this game, updates userBalance
 usersBalance = dealingCards.dealingCards(usersBet, usersBalance)
 print(f"Your new balance: {usersBalance}")
 
