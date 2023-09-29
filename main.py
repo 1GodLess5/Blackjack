e21f0b4c00d3c802bc7bc579cf793afdcbeec618
1GodLess5 <gacekalbert@seznam.cz>
2023-09-29 23:46:31 +0200

    moved functions ageChecker.main() and functions.rulesConfirmation() into account.py where they belong better now; added TODOs

--- main.py
+++ main.py
@@ -1,25 +1,20 @@
 import functions
 import components
-import ageChecker
 import dealingCards
-
+import account
 # welcome message, do you have an account?
 # no -> age verification, rules verification, create account
 # yes -> login, main menu -> 1) Play Game
 #                            2) Balance (> see my balance, top out my balance, withdraw)
 #                            3) Exit Game
 # TODO 1) you are in accountManagement branch
-# TODO    account.py in the bottom continue with TODO
+# TODO    account.py in the account() continue with TODOs
 
 # welcoming screen
 functions.firstScreen()
-# verifying user's age
-ageChecker.main()
-# user's agreement to the rules
-functions.rulesConfirmation()
+# account management
+userName, usersBalance = account.account()
 
-# getting user's balance to play with
-usersBalance = functions.enterBalance()  # testing variables: usersBalance = 500, usersBet = 25
 # getting user's bet for this round
 usersBet = functions.usersBet(usersBalance)
 # main function of this game, updates userBalance
