f570f1002e868a8675be1c3efb6a1bef184cf78a
1GodLess5 <gacekalbert@seznam.cz>
2023-09-30 16:07:39 +0200

    added TODOs

--- main.py
+++ main.py
@@ -1,21 +1,23 @@
+import os
 import functions
-import components
 import dealingCards
 import account
-# welcome message, do you have an account?
-# no -> age verification, rules verification, create account
-# yes -> login, main menu -> 1) Play Game
-#                            2) Balance (> see my balance, top out my balance, withdraw)
-#                            3) Exit Game
-# TODO 1) you are in accountManagement branch
-# TODO    account.py in the account() continue with TODOs
+
+# TODO 1) you are in usersMenu branch
+# TODO    menu.py continue with TODOs
+# FUTURE
+# TODO In the account.py create Forgot password -> After creating new account, create random string and pass it to
+# TODO the user, tell him to save it if he forgets the password. Hash the string and save it into new file "secrets.txt"
+# TODO when user forgets password, ask him for this code instead and make him create new password
 
 # welcoming screen
 functions.firstScreen()
 # account management
 userName, usersBalance = account.account()
-
+usersBalance = float(usersBalance)
 # getting user's bet for this round
+os.system("clear")
+functions.gameLogo()
 usersBet = functions.usersBet(usersBalance)
 # main function of this game, updates userBalance
 usersBalance = dealingCards.dealingCards(usersBet, usersBalance)
