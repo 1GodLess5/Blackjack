167e8755e0921d9d1cf9d95707a07ceb53b0b14e
1GodLess5 <gacekalbert@seznam.cz>
2023-10-17 00:09:34 +0200

    added TODOs, updating balance in users.txt\ now

--- main.py
+++ main.py
@@ -5,10 +5,7 @@ import account
 
 # TODO 1) you are in usersMenu branch
 # TODO    menu.py continue with TODOs
-# FUTURE
-# TODO In the account.py create Forgot password -> After creating new account, create random string and pass it to
-# TODO the user, tell him to save it if he forgets the password. Hash the string and save it into new file "secrets.txt"
-# TODO when user forgets password, ask him for this code instead and make him create new password
+
 
 # welcoming screen
 functions.firstScreen()
@@ -22,6 +19,6 @@ usersBet = functions.usersBet(usersBalance)
 # main function of this game, updates userBalance
 usersBalance = dealingCards.dealingCards(usersBet, usersBalance)
 print(f"Your new balance: {usersBalance}")
-
+functions.writeBalance(userName, usersBalance, True)
 
 
